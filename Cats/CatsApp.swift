98e6e936638d53d3434edf812aba84d5fc0a3cdd
Inna Lytvynenko <inna.lytvynenko@gen.tech>
2024-02-25 18:41:17 +0100

    Initial Commit

---
+++ Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by Inna Lytvynenko on 25.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
